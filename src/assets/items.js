94e6958c29f7e2b38f086e8de2fa18a45ded3be4
chikalin-ru <104888906+chikalin-ru@users.noreply.github.com>
2022-05-05 09:41:35 +0300

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,60 +1,66 @@
 import website from '../assets/website.png'
-import instagram from '../assets/instagram.png'
-import appstore from '../assets/playstore.png'
+import appstore from '../assets/appstore.jpg'
+import playstore from '../assets/playstore.png'
 import youtube from '../assets/youtube.jpeg'
-import dribbble from '../assets/dribbble.png'
-import github from '../assets/github.png'
 import telegram from '../assets/telegram.png'
-import twitter from '../assets/twitter.png'
+import filmfreeway from '../assets/filmfreeway.png'
+import imdb from '../assets/imdb.png'
+import justplay from '../assets/justplay.jpg'
+import ecorche from '../assets/ecorche.webp'
 
 const items = [{
-    "title": "Website",
+    "title": "Portfolio",
     "subtitle": "Look at my work!",
     "image": website,
-    "link": "https://sagniksahoo.codes" //your personal website or portfolio  link
+    "link": "https://portfolio.chikalin.ru"
 },
 {
-    "title": "GitHub",
-    "subtitle": "@heysagnik | 🏠 of my open-source projects",
-    "image": github,
-    "link": "https://github.com/heysagnik" //Github Profile link
+    "title": "Ecorche: Human Head Anatomy"
+    "subtitle": "ecorche.app",
+    "image": ecorche,
+    "link": "https://ecorche.app"
 },
 {
-    "title": "Instagram",
-    "subtitle": "@heysagnik | Hub of my awesome 😎 pics",
-    "image": instagram,
-    "link": "https://instagram.com/heysagnik" //instagram profile link 
+    "title": "Just Play"
+    "subtitle": "An animated short",
+    "image": justplay,
+    "link": "https://www.youtube.com/watch?v=pKdqm6nddFo"
 },
 {
-    "title": "Twitter",
-    "subtitle": "@heysagnik | Don't forget to follow me 😉",
-    "image": twitter,
-    "link": "https://twitter.com/heysagnik"// twitter profile link 
-},
-{
-    "title": "Apps",
-    "subtitle": "Hub of my awesome 🔥 Apps",
+    "title": "iOS Apps"
+    "subtitle": "All my iOS Apps",
     "image": appstore,
-    "link": "#"//
+    "link": "https://apps.apple.com/ru/developer/kirill-chikalin/id1525333206"
+},
+{
+    "title": "Android Apps"
+    "subtitle": "All my Android Apps",
+    "image": playstore,
+    "link": "https://play.google.com/store/apps/dev?id=9162788309736348460"
 },
 {
     "title": "YouTube",
-    "subtitle": "CODER SAGNIK | Official channel of mine",
+    "subtitle": "Kirill Chikalin | Official channel",
     "image": youtube,
-    "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"//youtube channel link 
+    "link": "https://www.youtube.com/c/KirillChikalin"
 },
-
 {
-    "title": "Dribbble",
-    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
-    "image": dribbble,
-    "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
+    "title": "FilmFreeway",
+    "subtitle": "Kirill Chikalin | independent filmmaker",
+    "image": filmfreeway,
+    "link": "https://filmfreeway.com/KirillChikalin"
+},
+{
+    "title": "IMDB",
+    "subtitle": "Kirill Chikalin | director, animator",
+    "image": imdb,
+    "link": "https://www.imdb.com/name/nm11577759"
 },
 {
     "title": "Telegram",
-    "subtitle": "@heysagnik | Chat with me instantly, Don't hesitate! ",
+    "subtitle": "@kirillchikalin",
     "image": telegram,
-    "link": "https://telegram.me/heysagnik" //Telegram Pofile 
+    "link": "https://telegram.me/kirillchikalin"
 }]
 
 export default items
